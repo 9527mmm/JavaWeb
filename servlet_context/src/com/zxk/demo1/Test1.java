bbe5acd80395beace1b0afc0db8db8880ebb4c37
9527mmm <45967247+9527mmm@users.noreply.github.com>
2021-07-21 22:22:59 +0800

    111

---
+++ servlet_context/src/com/zxk/demo1/Test1.java
@@ -0,0 +1,26 @@
+package com.zxk.demo1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @program: JavaWeb
+ * @description:
+ * @author: zhaoxuekai
+ * @GitHub: 9527mmm
+ * @Create: 2021-07-21 17:49
+ **/
+public class Test1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ servlet_context/web/index.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhaoxuekai
+  Date: 2021/7/21
+  @Create: 2021-07-21 17:48
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
